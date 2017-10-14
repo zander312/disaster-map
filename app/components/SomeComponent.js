f34fd6caf87982baa3065699a7cb66dfca784cc4
EC2 Default User <ec2-user@ip-172-31-12-192.ec2.internal>
2017-10-14 04:07:18 +0000

    adds simple form for user data

--- app/components/SomeComponent.js
+++ app/components/SomeComponent.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import styles from './SomeComponent.scss';
-import logo from '../../assets/1000-secret-lives-cows-1.jpg';
-import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import React from 'react'
+import s from './SomeComponent.scss'
+import axios from 'axios'
+import {
+  Button,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Grid,
+  Row,
+  Col,
+  Panel
+} from 'react-bootstrap'
 
 export default class SomeComponent extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { animals: '' };
+    super(props)
+    this.state = { animals: '' }
   }
 
   componentDidMount() {
@@ -15,20 +23,73 @@ export default class SomeComponent extends React.Component {
       .get('/dynamo/putuser')
       .then(
         function(res) {
-          console.log(res);
-          this.setState({ animals: res.data.animals });
+          console.log(res)
+          this.setState({ animals: res.data.animals })
         }.bind(this)
       )
       .then(function(err) {
-        console.log(err);
-      });
+        console.log(err)
+      })
   }
 
   render() {
     return (
       <div>
-        <Button bsStyle="primary">Primary</Button>
+        <Grid>
+          <Row>
+            <Col>
+              <form>
+                <Panel
+                  header="input your data"
+                  bsStyle="primary"
+                  className={s.panel}
+                >
+                  <FieldGroup
+                    id="formBasicText"
+                    label="label for input"
+                    type="text"
+                    value={'oi'}
+                    placeholder="Enter text"
+                    onChange={console.log('oi')}
+                  />
+                  <FieldGroup
+                    id="formBasicText"
+                    label="label for input"
+                    type="text"
+                    value={'oi'}
+                    placeholder="Enter text"
+                    onChange={console.log('oi')}
+                  />
+                  <FieldGroup
+                    id="formBasicText"
+                    label="label for input"
+                    type="text"
+                    value={'oi'}
+                    placeholder="Enter text"
+                    onChange={console.log('oi')}
+                  />
+                  <FieldGroup
+                    id="formBasicText"
+                    label="label for input"
+                    type="text"
+                    value={'oi'}
+                    placeholder="Enter text"
+                    onChange={console.log('oi')}
+                  />
+                  <Button type="submit">Submit</Button>
+                </Panel>
+              </form>
+            </Col>
+          </Row>
+        </Grid>
       </div>
-    );
+    )
   }
 }
+
+const FieldGroup = ({ id, label, ...rest }) => (
+  <FormGroup controlId={id}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl {...rest} />
+  </FormGroup>
+)
--- lint.sh
+++ lint.sh
@@ -1,1 +1,2 @@
 prettier ./**/*.js --write --no-semi --single-quote
+prettier ./**/**/*.js --write --no-semi --single-quote
